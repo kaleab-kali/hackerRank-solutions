d3577e87572a8a119dfac3e4b19800626b8aaf95
kaliktg <86680782+kaliktg@users.noreply.github.com>
2022-10-26 11:58:07 +0000

    solved selctionsort problem

---
+++ sorting-selection sort/selection.py
@@ -0,0 +1,16 @@
+class Solution:
+    def select(self, arr, i):
+        max = arr[0]
+        idx = 0
+        for j in range(1,i+1):
+            if arr[j] > max:
+                idx = j
+                max = arr[j]
+        return idx
+    
+    def selectionSort(self, arr, n):
+        
+        for i in range(n-1,0, -1):
+            j = self.select(arr,i)
+    
+            arr[i] ,arr[j]  = arr[j], arr[i]
