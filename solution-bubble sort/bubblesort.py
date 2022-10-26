0b08c6c3f423a1267eb9be1030ae69eaa95a7ce1
kaliktg <86680782+kaliktg@users.noreply.github.com>
2022-10-26 10:53:07 +0000

    solved buublesort problem

---
+++ solution-bubble sort/bubblesort.py
@@ -0,0 +1,20 @@
+#bubble sort answer to hackerrank solution
+
+
+def countSwaps(a):
+    swapped = 0
+    for i in range(len(a)):
+        for j in range(0, len(a) - i - 1):
+            if a[j] > a[j + 1]:
+                # temp = a[j]
+                # a[j] = a[j+1]
+                # a[j+1] = temp
+                a[j], a[j+1] = a[j+1], a[j]
+                swapped += 1
+
+                
+    print(f"Array is sorted in {swapped} swaps.")
+    print(f"First Element: {a[0]}")
+    print(f"Last Element: {a[len(a)-1]}")
+    
+countSwaps([3,2,1])
